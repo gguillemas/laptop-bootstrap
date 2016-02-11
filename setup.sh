3662a4ad30af6f46b372f871497df88f2a3b37aa
user <user@debian>
2016-02-11 16:38:18 +0100

    Fix typo, change xinit permissions, add dependencies and improve the startup script.

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-USERNAME="$USER"
-
 # Elevate privileges.
 if [ $EUID != 0 ]; then
     su -c "bash $0" "$@"
@@ -20,17 +18,21 @@ apt-get dist-upgrade
 # Install packages.
 apt-get install -y \
 sudo vim xorg tmux bash-completion \
-wicd-curses chromium curl wget \
+wicd-curses curl wget chromium \
 pepperflashplugin-nonfree icedtea-plugin \
-git gcc golang ruby python build-essential \
-vlc feh \
+build-essential git golang ruby \
+vlc feh imagemagick \
 libreoffice evince \
 xfonts-terminus ttf-freefont ttf-mscorefonts-installer ttf-bitstream-vera ttf-dejavu ttf-liberation
 
-# Add user to sudoers.
-sed "s/root ALL=(ALL) ALL/&\n$USERNAME ALL=(ALL) ALL/" /etc/sudoers
+# Add user to sudo group.
+adduser "$USER" sudo
 
 # Install Suckless tools.
+apt-get install -y lib11-dev libxinerama-dev
 make -C sources/dwm/ && make install clean -C sources/dwm/
-make -C sources/dmemu/ && make install clean -C sources/dmenu/
+make -C sources/dmenu/ && make install clean -C sources/dmenu/
 make -C sources/slock/ && make install clean -C sources/slock/
+
+# Fix xinit permissions.
+chmod u+s /usr/bin/xinit
