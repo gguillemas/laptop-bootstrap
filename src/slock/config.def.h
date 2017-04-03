f55ff1c226a259db384176f6e603056c73223c00
Gerard Guillemas Martos <gguillemas@gmail.com>
2017-04-03 19:57:29 +0200

    Update slock to 1.4

--- src/slock/config.def.h
+++ src/slock/config.def.h
@@ -1,2 +1,12 @@
-#define COLOR1 "black"
-#define COLOR2 "#005577"
+/* user and group to drop privileges to */
+static const char *user  = "nobody";
+static const char *group = "nogroup";
+
+static const char *colorname[NUMCOLS] = {
+	[INIT] =   "#000000",   /* after initialization */
+	[INPUT] =  "#FFFFFF",   /* during input */
+	[FAILED] = "#FF0000",   /* wrong password */
+};
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 1;
